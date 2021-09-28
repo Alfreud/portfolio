aa7410dc4d0ebc78943b83be19c4f6cc84ec301a
Alfred0 GA <alfred007ga@gmail.com>
2021-09-27 23:16:44 -0500

    Portfolio Component Completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
-import Experience from "./components/Experience"
+import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
       <AboutMe />
       <Services />
       <Experience />
+      <Portfolio />
     </>
   );
 }
